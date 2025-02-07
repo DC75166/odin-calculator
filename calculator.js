aae7b01941bbfa6bd7fe8ba078f9a3369ee520be
DC75166 <daipayanchakraborty75@gmail.com>
2025-02-07 07:31:24 +0530

    created logic for number display - not working

--- calculator.js
+++ calculator.js
@@ -1,34 +1,42 @@
 function calculator() {
-  const num1 = document.querySelector("#num1");
-  const num2 = document.querySelector("#num2");
-  const operator = document.querySelector("#operator");
+  const buttons = document.querySelector(".btn");
+  const display = document.querySelector("#display");
+  // const operator = document.querySelector("#operator");
+  let num1 = "";
+  let num2 = "";
+  let operator = "";
 
-  function add(a, b) {
-    console.log(a + b);
-  }
-  function subtract(a, b) {
-    console.log(a - b);
-  }
-  function multiply(a, b) {
-    console.log(a * b);
-  }
-  function divide(a, b) {
-    console.log(a / b);
-  }
 
   function operate(num1, num2, operator) {
     if (operator === "+") {
-      add(num1, num2);
+      return num1+num2;
     } else if (operator === "-") {
-      subtract(num1, num2);
+      return num1-num2;
     } else if (operator === "*") {
-      multiply(num1, num2);
+      return num1*num2;
     } else if (operator === "/") {
-      divide(num1, num2);
+      return num1/num2;
     }
   }
 
-  operate(num1, num2, operator);
+  function show() {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        const value = buttons[i].textContent;
+        if (value >= "0" && value <= "9") {
+          if (operator) {
+            num2 += value;
+          } else {
+            num1 += value;
+          }
+          display.textContent += value;
+      }
+    });
+  }
+}
+  
+  show();
+  // operate(num1, num2, operator);
 }
 
 calculator();
